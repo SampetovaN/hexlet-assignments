e70a4b08502e0791339e31f2c1204d556c4811b9
@hexlet/cli <support@hexlet.io>
2023-01-14 22:14:28 +0200

    submit java-collections-ru/other-collections

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -5,32 +5,26 @@ import java.util.stream.Collectors;
 
 // BEGIN
 class App {
-    static public LinkedHashMap<String, String> genDiff (Map<String, Object> map1, Map<String, Object> map2) {
-        TreeSet<String> set = new TreeSet<>();
-        for (String key : map1.keySet()) {
-            if(map1.get(key) != null && map2.get(key) == null) {
-                set.add(key + "=deleted");
-            }
-            else if (!map1.get(key).equals(map2.get(key))) {
-                set.add(key + "=changed");
-            }
+    public static Map<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
 
-            else if(map1.get(key).equals(map2.get(key))) {
-                set.add(key + "=unchanged");
+        Map<String, String> result = new LinkedHashMap<>();
+        Set<String> keys = new TreeSet<>(data1.keySet());
+        keys.addAll(data2.keySet());
+
+        for (String key: keys) {
+
+            if (!data1.containsKey(key)) {
+                result.put(key, "added");
+            } else if (!data2.containsKey(key)) {
+                result.put(key, "deleted");
+            } else if (data1.get(key).equals(data2.get(key))) {
+                result.put(key, "unchanged");
+            } else {
+                result.put(key, "changed");
             }
         }
-        for (String key: map2.keySet()) {
-            if(map2.get(key) != null && map1.get(key) == null) {
-                set.add(key + "=added");
-            }
-        }
 
-
-      return set.stream()
-              .collect(Collectors.toMap(
-                      k -> k.split("=")[0],
-                      v -> v.split("=")[1],
-                      (o1, o2) -> o2, LinkedHashMap::new));
+        return result;
     }
 }
 //END
