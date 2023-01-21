fd54081d040f7a4caa5585f7a215f5badd42e39c
@hexlet/cli <support@hexlet.io>
2023-01-21 17:23:53 +0200

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -5,42 +5,38 @@ import java.util.Map;
 
 // BEGIN
 class FileKV implements KeyValueStorage {
-    private String path;
-    private Map<String, String> map;
-    private Map<String, String> allData = new HashMap<>();
 
-    public FileKV(String path, Map<String, String> map) {
-        this.path = path;
-        this.map = new HashMap<>(map);
-        this.allData.putAll(this.map);
-        write();
+    private String filepath;
+
+    FileKV(String filepath, Map<String, String> initial) {
+        this.filepath = filepath;
+        initial.entrySet().stream().forEach(entry -> set(entry.getKey(), entry.getValue()));
     }
 
-    private void write() {
-        Utils.writeFile(this.path, this.allData.toString());
-    }
-
-    @Override
     public void set(String key, String value) {
-        this.allData.put(key, value);
-        write();
+        String content = Utils.readFile(filepath);
+        Map<String, String> data = Utils.unserialize(content);
+        data.put(key, value);
+        Utils.writeFile(filepath, Utils.serialize(data));
     }
 
-    @Override
     public void unset(String key) {
-        this.allData.remove(key);
-        write();
+        String content = Utils.readFile(filepath);
+        Map<String, String> data = Utils.unserialize(content);
+        data.remove(key);
+        Utils.writeFile(filepath, Utils.serialize(data));
     }
 
-    @Override
     public String get(String key, String defaultValue) {
-        var value = this.allData.get(key);
-        return (value == null || value.isEmpty()) ? defaultValue : value;
+        String content = Utils.readFile(filepath);
+        Map<String, String> data = Utils.unserialize(content);
+        return data.containsKey(key) ? data.get(key) : defaultValue;
     }
 
-    @Override
     public Map<String, String> toMap() {
-        return new HashMap<>(this.allData);
+        String content = Utils.readFile(filepath);
+        Map<String, String> data = Utils.unserialize(content);
+        return data;
     }
 }
 // END
